b0bea1e4ebb8c5ce7967b8cdcd2d206563c054b8
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-05 15:27:43 +0200

    created the Rectangle object using Function Constructor

--- Lab-1/1-Rectangle/rect.js
+++ Lab-1/1-Rectangle/rect.js
@@ -1,3 +1,5 @@
+// Anonymous Way
+
 var rectangle = {
   width: 13,
   height: 6,
@@ -26,3 +28,40 @@ var rectangle = {
 };
 
 document.getElementById("info").innerHTML = rectangle.displayInfo();
+
+// Function Constructor Way
+
+function Rectangle(w, h) {
+  this.width = w;
+  this.height = h;
+  this.area = function () {
+    return this.width * this.height;
+  };
+
+  this.perimeter = function () {
+    return (this.width + this.height) * 2;
+  };
+
+  this.displayInfo = function () {
+    return (
+      "<h1>The rectangle info:</h1> <br />" +
+      "<b>The width: </b>" +
+      this.width +
+      "<br />" +
+      "<b>The height: </b>" +
+      this.height +
+      "<br />" +
+      "<b>The area of the rectangle: </b>" +
+      this.area() +
+      "<br />" +
+      "<b>The perimeter of the rectangle: </b>" +
+      this.perimeter()
+    );
+  };
+}
+
+var rect1 = new Rectangle(2, 3);
+var rect2 = new Rectangle(5, 6);
+
+// document.getElementById("info").innerHTML = rect1.displayInfo();
+document.getElementById("info").innerHTML = rect2.displayInfo();
