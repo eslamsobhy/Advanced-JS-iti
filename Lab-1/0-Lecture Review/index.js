2312642f1622bdeb5f003879cfd4af53433fda39
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-05 17:42:27 +0200

    reviewed the accessor descriptor

--- Lab-1/0-Lecture Review/index.js
+++ Lab-1/0-Lecture Review/index.js
@@ -4,7 +4,7 @@
 ---------------------------
 */
 
-var obj = { age: 25 };
+var obj = { age: 25, bonus: 200 };
 
 Object.defineProperty(obj, "name", {
   value: "Eslam", // the value of the key (i.e. "name")
@@ -17,7 +17,38 @@ obj.name = "Ahmed"; // applied since set to true
 delete obj.name; // not applied since set to false
 for (key in obj) {
   // name not iteratable since set to false
-  console.log(key);
+  //   console.log(key);
 }
 
-console.log(obj);
+// console.log(obj);
+
+/*
+---------------------------
+|ACCESSOR DESCRIPTIVE WAY |
+---------------------------
+*/
+(function () {
+  var sal = 3000;
+  Object.defineProperty(obj, "salary", {
+    get: function () {
+      return sal + this.bonus; // the value of the salary property
+      // return undefined by default
+    },
+    set: function (value) {
+      if (typeof value !== "number") {
+        throw "ERROR: Salary must have a numeric value!";
+      }
+      // this.salary = value; // ERROR: infinite loop!
+      sal = value;
+    },
+    configurable: false,
+    enumerable: true,
+  });
+})();
+
+// console.log(obj.salary); // undefined since get returns undefined by default
+// console.log(obj.salary); // getter
+
+// not applied! still the salary has the old value!
+obj.salary = 4800; // setter (this calls the set() method)
+console.log(obj.salary);
