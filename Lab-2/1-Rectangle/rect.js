5f2686b8d5c80be2230159ab2833fb1a9aa57ef9
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-10 23:20:33 +0200

    overrode the toString method

--- Lab-2/1-Rectangle/rect.js
+++ Lab-2/1-Rectangle/rect.js
@@ -11,5 +11,22 @@ Rectangle.prototype.perimeter = function () {
   return (this.width + this.height) * 2;
 };
 
+Rectangle.prototype.toString = function () {
+  document.write(
+    "<b>Width:</b> " +
+      this.width +
+      "<br />" +
+      "<b>Height:</b> " +
+      this.height +
+      "<br />" +
+      "<b>Area:</b> " +
+      this.area() +
+      "<br />" +
+      "<b>Perimeter:</b> " +
+      this.perimeter() +
+      "<br />"
+  );
+};
+
 var rectObj = new Rectangle(5, 7);
-console.log(rectObj.perimeter());
+rectObj.toString();
