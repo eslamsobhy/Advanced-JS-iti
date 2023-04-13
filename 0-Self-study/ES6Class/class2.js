10559538ed9b2492e7ea635e8fb1a11a8f39b8dc
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-13 23:44:36 +0200

    implemented Encapsulation with methods and properties

--- 0-Self-study/ES6Class/class2.js
+++ 0-Self-study/ES6Class/class2.js
@@ -3,28 +3,36 @@
 // - Allows internal changes without breaking the outside code!
 
 class Account {
+  // Public fields (available in the instances)
+  locale = navigator.language;
+
+  // Private fields (available in the instances)
+  #movements = [];
+  #pin; // Fields can't be declared in the constructor
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin; // protected property
+    this.#pin = pin; // protected property (now it is private)
     // two properties that do not need data when creating the object
-    this._movements = []; // Protected property (by convention)
-    this.locale = navigator.language;
+    // this._movements = []; // Protected property (by convention)
+    // this.locale = navigator.language;
     // Every time we create an instance, this happens:
     console.log(`Thanks ${owner} for creating an account!`);
   }
 
+  // Public Methods
   // Public interface
   // Public interface is used to prevent manipulating the properties directly, instead via methods.
   // also called APIs.
   getMovements() {
     // this method is created to get the movements data outside the class,
     // since it is protected and can't be accessed otside the class. (By convention)
-    return this._movements;
+    return this.#movements;
   }
 
   deposite(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
@@ -32,21 +40,29 @@ class Account {
   }
 
   // Protected method
-  _approveLoan(val) {
-    // this should have some logic
-    return true;
-  }
+  //   _approveLoan(val) {
+  //     // this should have some logic
+  //     return true;
+  //   }
 
   requestLoan(val) {
-    if (this._approveLoan(val)) {
+    if (this.#approveLoan(val)) {
       this.deposite(val);
       console.log("Loan Accepted!");
     }
+  }
+
+  // Protected Mothods
+  #approveLoan(val) {
+    return true;
   }
 }
 
 const acc1 = new Account("Eslam", "EGP", 1112);
 acc1.deposite(250);
 acc1.withdraw(140);
+acc1.requestLoan(400);
 console.log(acc1.getMovements());
+// console.log(#approveLoan(400)); // ERROR: Can't be accessed outside of the class
+// console.log(acc1.#movements); // ERROR: Can't be accessed outside of the class
 console.log(acc1);
