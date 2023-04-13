04d802d8647c26f256929d27717e52a0c792a555
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-13 23:21:13 +0200

    made some properties and methods protected (By convention)

--- 0-Self-study/ES6Class/class2.js
+++ 0-Self-study/ES6Class/class2.js
@@ -6,9 +6,9 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
+    this._pin = pin; // protected property
     // two properties that do not need data when creating the object
-    this.movements = [];
+    this._movements = []; // Protected property (by convention)
     this.locale = navigator.language;
     // Every time we create an instance, this happens:
     console.log(`Thanks ${owner} for creating an account!`);
@@ -17,21 +17,28 @@ class Account {
   // Public interface
   // Public interface is used to prevent manipulating the properties directly, instead via methods.
   // also called APIs.
+  getMovements() {
+    // this method is created to get the movements data outside the class,
+    // since it is protected and can't be accessed otside the class. (By convention)
+    return this._movements;
+  }
+
   deposite(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposite(-val);
   }
 
-  approveLoan(val) {
+  // Protected method
+  _approveLoan(val) {
     // this should have some logic
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposite(val);
       console.log("Loan Accepted!");
     }
@@ -41,4 +48,5 @@ class Account {
 const acc1 = new Account("Eslam", "EGP", 1112);
 acc1.deposite(250);
 acc1.withdraw(140);
+console.log(acc1.getMovements());
 console.log(acc1);
