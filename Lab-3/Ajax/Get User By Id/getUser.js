90ba9a2404955903b9c17982cc1cec505ed7a1e3
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-14 23:27:04 +0200

    handled some error checks

--- Lab-3/Ajax/Get User By Id/getUser.js
+++ Lab-3/Ajax/Get User By Id/getUser.js
@@ -3,6 +3,14 @@ const inputField = document.querySelector("input");
 
 function save() {
   const id = inputField.value;
+  //   checking if the user didn't enter a user is
+  if (!id) {
+    throw "ERROR: Please enter the user id!";
+  }
+  //   checking if the user a entered a non-numerical value
+  else if (!isFinite(id)) {
+    throw "ERROR: The user ID must be a number!";
+  }
   fetchData(id);
 }
 
@@ -12,11 +20,15 @@ function fetchData(id) {
   request.send();
 
   request.addEventListener("load", function () {
+    //   if the user is not found!
+    if (this.status === 404) {
+      throw "ERROR: The user is not found!";
+    }
     const { data } = JSON.parse(request.response);
     userContainer.innerHTML = `<img src=${data.avatar} alt="${data.first_name} image" />
                                 <div class="user-name">${data.first_name} ${data.last_name}</div>`;
 
     userContainer.style.opacity = 1;
-    console.log(data);
+    // console.log(data);
   });
 }
