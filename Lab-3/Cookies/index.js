e397e48d7f45d2910337bc41be071c306f012838
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-04-25 06:50:53 +0200

    implemented reading the cookie

--- Lab-3/Cookies/index.js
+++ Lab-3/Cookies/index.js
@@ -7,6 +7,19 @@ function setCookie(cookieKey, cookieValue, expDate) {
     : `${cookieKey}=${cookieValue}`;
 }
 
-var expDate = new Date("4-25-2023");
+function getCookie(cookieKey) {
+  if (!cookieKey) throw "ERROR: Please enter the cookie key as a parameter!";
+  var cookieData = document.cookie;
+  var keys = cookieData.split(";");
+  var cookie = keys.filter((key) => key.includes(cookieKey));
+  if (!cookie.length) throw `ERROR: Cookie (${cookieKey}) is not found!`;
+  var value = cookie[0].trim().split("=")[1];
+  return value;
+}
+
+var expDate = new Date(2023, 3, 26);
+setCookie("youssef", 25);
 setCookie("name", "Eslam Sobhy", expDate);
 setCookie("age", 24, expDate);
+
+console.log(getCookie("youssef"));
